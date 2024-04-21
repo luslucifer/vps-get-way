bb7668cfcb26de960aed011457157ca06cf3754c
luslucifer <100895428+luslucifer@users.noreply.github.com>
2024-04-21 22:09:32 +0600

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ def home():
     url = request.args.get('url')
     if url == None :   
         return ' i am mr k . i gonna fuck you real soon '
-    res = requests.get(url).content
+    res = requests.get('https://moviekex.online:5001/fetch?url='+url).content
     return res
 
 
