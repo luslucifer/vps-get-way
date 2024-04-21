63c194a7cbe7e7780c4af9151332de63077398ce
luslucifer <100895428+luslucifer@users.noreply.github.com>
2024-04-21 21:57:44 +0600

    first commit

---
+++ main.py
@@ -0,0 +1,21 @@
+import requests
+from flask import Flask,request
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home(): 
+    url = request.args.get('url')
+    if url == None :   
+        return ' i am mr k . i gonna fuck you real soon '
+    res = requests.get(url).content
+    return res
+
+
+if __name__ == '__main__': 
+    app.run()
+
+
+
